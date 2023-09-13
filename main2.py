c9439046fb6efa78bf491c004a61e4a6e10b4b56
Faris <farisrendra@gmail.com>
2023-09-13 18:39:24 +0700

    First demo project

---
+++ main2.py
@@ -0,0 +1,9 @@
+"""
+Basic syntax demo
+"""
+#Sequential
+print('Mom said, "Go to the shop"')
+print('Budi said, "Okay what should i do there?"')
+print('Mom said, "Buy 1 bottle of milk and if they had eggs, buy 6"')
+print('Then Budi go to the shop')
+print('And starts shopping')
