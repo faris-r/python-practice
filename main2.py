bed857caaf4235aff8ec436efe2e01763af907d4
Faris <farisrendra@gmail.com>
2023-09-13 22:01:07 +0700

    Continue..

--- main2.py
+++ main2.py
@@ -1,9 +1,26 @@
 """
 Basic syntax demo
 """
-#Sequential
+# Sequential
+
 print('Mom said, "Go to the shop"')
 print('Budi said, "Okay what should i do there?"')
 print('Mom said, "Buy 1 bottle of milk and if they had eggs, buy 6"')
-print('Then Budi go to the shop')
-print('And starts shopping')
+print('Then Budi go to the store')
+
+# Branch
+milk_bottle_count = 20
+egg_count = 0
+print(f"There are {milk_bottle_count} bottles")
+print(f"There are {egg_count} eggs")
+
+if milk_bottle_count > 0:
+    print("Budi checks the price, and his money is enough")
+    if egg_count == 0:
+        print("Budi buys 1 milk bottle")
+    else:
+        print("Budi buys 6 milk bottles")
+else:
+    print("Budi doesn't buy 1 milk bottle")
+print("Budi goes home")
+print("Budi gives the result to his mom")
