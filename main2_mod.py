7ed95e160aaf8f608c215ee5572f5eb4e00228c4
Faris <farisrendra@gmail.com>
2023-09-13 22:40:41 +0700

    Conditional

--- main2_mod.py
+++ main2_mod.py
@@ -10,18 +10,19 @@ print('Then Budi go to the store')
 
 # Branch
 milk_bottle_count = 4
-egg_count = 2
+egg_count = 6
 print(f"There are {milk_bottle_count} bottles")
 print(f"There are {egg_count} eggs")
 
 if milk_bottle_count > 0:
     print("Budi checks the price, and his money is enough")
-    if egg_count == 0:
-        print("Budi buys 1 milk bottle")
-    else:
-        print("Budi buys 6 milk bottles")
+    print("Budi buys 1 milk bottle")
 else:
     print("Budi doesnt buy milk")
+if egg_count < 6:
+    print(f"Budi buys {egg_count} egg(s)")
+else:
+    print("Budi buys 6 eggs")
 
 print("Budi goes home")
 print("Budi gives the result to his mom")
