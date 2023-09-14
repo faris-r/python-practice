338d9f5060c28acc3b4c3d57f3882a1c577ed729
Faris <farisrendra@gmail.com>
2023-09-15 00:18:26 +0700

    Looping using for and while for certain number of books

---
+++ test.py
@@ -0,0 +1,17 @@
+"""
+This is example of basic looping using while w/ undefined count
+"""
+books_count = 10
+print("Teacher asked Joe to read & understand all of his books")
+
+books_read_and_understood = 0
+print(f"The number of books that Joe has read is {books_read_and_understood} book(s)")
+
+while books_read_and_understood < books_count:
+    if books_read_and_understood == 10:
+        print(f"Joe hasn't understood book {books_read_and_understood}")
+    else:
+        books_read_and_understood = books_read_and_understood + 1
+        print(f"Joe has read & understood book {books_read_and_understood}")
+
+print(f"The number of books that Joe has read is {books_read_and_understood} books")
